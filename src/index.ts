a467a88a8ff9359f1f926fe5d42c1d5247f750b1
Olaf Haalstra <Olaf Haalstra>
2017-01-29 00:26:59 +0100

    feat(index.ts): Prompt install
    Installer asks to install stryker if not instaled. Added some nice colors for readability.

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as fs from 'fs';
 import * as path from 'path';
 import * as chalk from 'chalk';
+import * as inquirer from 'inquirer';
 
 const NODE_MODULES_DIRNAME = 'node_modules';
 const STRYKER_DIRNAME = 'stryker';
@@ -8,30 +9,33 @@ const BIN_DIRNAME = 'bin';
 const STRYKER_FILENAME = 'stryker';
 
 function fileExists(filePath: string) {
-    try {
-        return fs.statSync(filePath).isFile();
-    } catch (error) {
-        return false;
-    }
-}
-
-function strykerNotFound() {
-    console.error(chalk.red.bold('Cannot find Stryker!\nPlease install Stryker using "npm install stryker".'));
-    process.exit(1);
+  try {
+    return fs.statSync(filePath).isFile();
+  } catch (error) {
+    return false;
+  }
 }
 
 const baseDir = process.cwd();
+
 const strykerPathFromNode = path.resolve(baseDir, NODE_MODULES_DIRNAME, STRYKER_DIRNAME, BIN_DIRNAME, STRYKER_FILENAME);
 const strykerPathFromBuild = path.resolve(baseDir, BIN_DIRNAME, STRYKER_FILENAME);
 
-try {
-    if (fileExists(strykerPathFromNode)) {
-        require(strykerPathFromNode);
-    } else if (fileExists(strykerPathFromBuild)) {
-        require(strykerPathFromBuild);
+if (fileExists(strykerPathFromNode)) {
+  require(strykerPathFromNode);
+} else if (fileExists(strykerPathFromBuild)) {
+  require(strykerPathFromBuild);
+} else {
+  console.log(chalk.yellow('Stryker is currently not installed.'));
+  inquirer.prompt([{ type: 'confirm', name: 'install', message: 'Do you want to automatically install Stryker?', default: 'true' }]).then((answers) => {
+    if (answers['install']) {
+      // Install stryker;
+      require('child_process').execSync('npm i --save-dev stryker stryker-api');
+      console.log(chalk.green('Stryker installation done.'));
+      console.log('Get started by using ' + chalk.blue('`stryker init`') + '.');
     } else {
-        strykerNotFound();
+      console.log('I understand. You can install Stryker manually using `npm install stryker`.');
     }
-} catch (error) {
-    strykerNotFound();
-} 
+  })
+}
+

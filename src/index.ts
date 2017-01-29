c98da96bf5ed1d5c65417a146fd7ab577aaef253
OlafHaalstra <olaf_haalstra@hotmail.com>
2017-01-29 12:05:02 +0100

    feat: Stryker ASCII Art
    Remove Stryker-API from peerDependencies as preparation for NPM
    Add Stryker ASCII Art

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as chalk from 'chalk';
 import * as inquirer from 'inquirer';
+import * as child from 'child_process';
 
 const NODE_MODULES_DIRNAME = 'node_modules';
 const STRYKER_DIRNAME = 'stryker';
@@ -21,6 +22,28 @@ const baseDir = process.cwd();
 const strykerPathFromNode = path.resolve(baseDir, NODE_MODULES_DIRNAME, STRYKER_DIRNAME, BIN_DIRNAME, STRYKER_FILENAME);
 const strykerPathFromBuild = path.resolve(baseDir, BIN_DIRNAME, STRYKER_FILENAME);
 
+const strykerASCII = 
+'\n' + 
+chalk.yellow('             |STRYKER|              ') + '\n' + 
+chalk.yellow('       ~control the mutants~        ') + '\n' + '\n' + 
+chalk.red('           ..####') + chalk.white('@')  +  chalk.red('####..            ') + '\n' + 
+chalk.red('        .########') + chalk.white('@') + chalk.red('########.         ') + '\n' + 
+chalk.red('      .#####################.       ') + '\n' + 
+chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' + 
+chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
+chalk.red('    #########') + chalk.yellow('##') + chalk.red('################     ') + '\n' + 
+chalk.red('    ') + chalk.white('@@@') + chalk.red('#######') + chalk.yellow('#######') + chalk.red('#######') + chalk.white('@@@') + chalk.red('     ') + '\n' + 
+chalk.red('    ################') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
+chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
+chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' + 
+chalk.red(`      '######################'      `) + '\n' +  
+chalk.red(`        '########`) + chalk.white('@') + chalk.red(`#########'        `) + '\n' +  
+chalk.red(`           ''####`) + chalk.white('@') + chalk.red(`####''            `) + '\n';
+
+
+
+console.log(strykerASCII);
+
 if (fileExists(strykerPathFromNode)) {
   require(strykerPathFromNode);
 } else if (fileExists(strykerPathFromBuild)) {
@@ -30,12 +53,12 @@ if (fileExists(strykerPathFromNode)) {
   inquirer.prompt([{ type: 'confirm', name: 'install', message: 'Do you want to automatically install Stryker?', default: 'true' }]).then((answers) => {
     if (answers['install']) {
       // Install stryker;
-      require('child_process').execSync('npm i --save-dev stryker stryker-api');
+      child.execSync('npm i --save-dev stryker stryker-api', {stdio: [0, 1, 2]});
       console.log(chalk.green('Stryker installation done.'));
       console.log('Get started by using ' + chalk.blue('`stryker init`') + '.');
     } else {
       console.log('I understand. You can install Stryker manually using `npm install stryker`.');
     }
-  })
+  });
 }
 

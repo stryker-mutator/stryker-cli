d5aaeb700db5d2568d7eeb6d407e3d262ce48c04
Nico Korthout <korthout@users.noreply.github.com>
2017-01-29 21:22:43 +0100

    chore(stryker-cli.ts): Improve reabability (#11)
    Separate code in functions.
    
    Linewrap some long lines
    
    And print ASCII only at install Stryker, not at all cli commands

--- src/stryker-cli.ts
+++ src/stryker-cli.ts
@@ -9,6 +9,20 @@ const STRYKER_DIRNAME = 'stryker';
 const BIN_DIRNAME = 'bin';
 const STRYKER_FILENAME = 'stryker';
 
+function start() {
+  const workingDirectory = process.cwd();
+  const strykerPathFromNode = path.resolve(workingDirectory, NODE_MODULES_DIRNAME, STRYKER_DIRNAME, BIN_DIRNAME, STRYKER_FILENAME);
+  const strykerPathFromBuild = path.resolve(workingDirectory, BIN_DIRNAME, STRYKER_FILENAME);
+
+  if (fileExists(strykerPathFromNode)) {
+    require(strykerPathFromNode);
+  } else if (fileExists(strykerPathFromBuild)) {
+    require(strykerPathFromBuild);
+  } else {
+    promptInstallStryker();
+  }
+}
+
 function fileExists(filePath: string) {
   try {
     return fs.statSync(filePath).isFile();
@@ -17,47 +31,48 @@ function fileExists(filePath: string) {
   }
 }
 
-const baseDir = process.cwd();
-
-const strykerPathFromNode = path.resolve(baseDir, NODE_MODULES_DIRNAME, STRYKER_DIRNAME, BIN_DIRNAME, STRYKER_FILENAME);
-const strykerPathFromBuild = path.resolve(baseDir, BIN_DIRNAME, STRYKER_FILENAME);
-
-const strykerASCII = 
-'\n' + 
-chalk.yellow('             |STRYKER|              ') + '\n' + 
-chalk.yellow('       ~control the mutants~        ') + '\n' + '\n' + 
-chalk.red('           ..####') + chalk.white('@')  +  chalk.red('####..            ') + '\n' + 
-chalk.red('        .########') + chalk.white('@') + chalk.red('########.         ') + '\n' + 
-chalk.red('      .#####################.       ') + '\n' + 
-chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' + 
-chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
-chalk.red('    #########') + chalk.yellow('##') + chalk.red('################     ') + '\n' + 
-chalk.red('    ') + chalk.white('@@@') + chalk.red('#######') + chalk.yellow('#######') + chalk.red('#######') + chalk.white('@@@') + chalk.red('     ') + '\n' + 
-chalk.red('    ################') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
-chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
-chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' + 
-chalk.red(`      '######################'      `) + '\n' +  
-chalk.red(`        '########`) + chalk.white('@') + chalk.red(`#########'        `) + '\n' +  
-chalk.red(`           ''####`) + chalk.white('@') + chalk.red(`####''            `) + '\n';
-
-
-
-if (fileExists(strykerPathFromNode)) {
-  require(strykerPathFromNode);
-} else if (fileExists(strykerPathFromBuild)) {
-  require(strykerPathFromBuild);
-} else {
-  console.log(strykerASCII);
+function promptInstallStryker() {
   console.log(chalk.yellow('Stryker is currently not installed.'));
-  inquirer.prompt([{ type: 'confirm', name: 'install', message: 'Do you want to automatically install Stryker?', default: 'true' }]).then((answers) => {
+  inquirer.prompt([{
+    type: 'confirm',
+    name: 'install',
+    message: 'Do you want to automatically install Stryker?',
+    default: 'true'
+  }]).then((answers) => {
     if (answers['install']) {
-      // Install stryker;
-      child.execSync('npm i --save-dev stryker stryker-api', {stdio: [0, 1, 2]});
-      console.log(chalk.green('Stryker installation done.'));
-      console.log('Get started by using ' + chalk.blue('`stryker init`') + '.');
+      installStryker();
     } else {
       console.log('I understand. You can install Stryker manually using `npm install stryker`.');
     }
   });
 }
 
+function installStryker() {
+  printStrykerASCII();
+  child.execSync('npm i --save-dev stryker stryker-api', {stdio: [0, 1, 2]});
+  console.log(chalk.green('Stryker installation done.'));
+  console.log(`Get started by using ${chalk.blue('`stryker --help`')}.`);
+}
+
+function printStrykerASCII() {
+  const strykerASCII = 
+  '\n' + 
+  chalk.yellow('             |STRYKER|              ') + '\n' + 
+  chalk.yellow('       ~control the mutants~        ') + '\n' + '\n' + 
+  chalk.red('           ..####') + chalk.white('@')  +  chalk.red('####..            ') + '\n' + 
+  chalk.red('        .########') + chalk.white('@') + chalk.red('########.         ') + '\n' + 
+  chalk.red('      .#####################.       ') + '\n' + 
+  chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' + 
+  chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
+  chalk.red('    #########') + chalk.yellow('##') + chalk.red('################     ') + '\n' + 
+  chalk.red('    ') + chalk.white('@@@') + chalk.red('#######') + chalk.yellow('#######') + chalk.red('#######') + chalk.white('@@@') + chalk.red('     ') + '\n' + 
+  chalk.red('    ################') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
+  chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
+  chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' + 
+  chalk.red(`      '######################'      `) + '\n' +  
+  chalk.red(`        '########`) + chalk.white('@') + chalk.red(`#########'        `) + '\n' +  
+  chalk.red(`           ''####`) + chalk.white('@') + chalk.red(`####''            `) + '\n';
+  console.log(strykerASCII);
+}
+
+start();

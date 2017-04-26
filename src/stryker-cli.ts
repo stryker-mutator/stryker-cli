4c141d5b2848ddc4ce8e3b72be1b08673fa8b5ba
Alex van Assem <avassem@gmail.com>
2017-04-26 14:44:13 +0200

    Refactored cli with resolve module

--- src/stryker-cli.ts
+++ src/stryker-cli.ts
@@ -3,32 +3,33 @@ import * as path from 'path';
 import * as chalk from 'chalk';
 import * as inquirer from 'inquirer';
 import * as child from 'child_process';
+import * as resolver from 'resolve';
 
-const NODE_MODULES_DIRNAME = 'node_modules';
-const STRYKER_DIRNAME = 'stryker';
-const BIN_DIRNAME = 'bin';
-const STRYKER_FILENAME = 'stryker';
-
-function start() {
-  const workingDirectory = process.cwd();
-  const strykerPathFromNode = path.resolve(workingDirectory, NODE_MODULES_DIRNAME, STRYKER_DIRNAME, BIN_DIRNAME, STRYKER_FILENAME);
-  const strykerPathFromBuild = path.resolve(workingDirectory, BIN_DIRNAME, STRYKER_FILENAME);
-
-  if (fileExists(strykerPathFromNode)) {
-    require(strykerPathFromNode);
-  } else if (fileExists(strykerPathFromBuild)) {
-    require(strykerPathFromBuild);
-  } else {
+function run() {
+  findPathToProgram('stryker').then(function (pathToProgram: string) {
+    // Run npm program
+    require(pathToProgram);
+  }).catch(function (err) {
+    // Ask to automaticly install Stryker
     promptInstallStryker();
-  }
+  });
 }
 
-function fileExists(filePath: string) {
-  try {
-    return fs.statSync(filePath).isFile();
-  } catch (error) {
-    return false;
-  }
+function findPathToProgram(program: string): Promise<string> {
+  return new Promise((resolve, reject) => {
+    resolver(program, { basedir: process.cwd() }, (err, pathToNpmModule: string) => {
+      if (err) {
+        reject(err); return;
+      }
+
+      let pathToProgram = path.resolve(path.dirname(pathToNpmModule), '../bin/stryker');
+      if (fs.existsSync(pathToProgram)) {
+        resolve(pathToProgram);
+      } else {
+        reject('Cannot run stryker'); return;
+      } 
+    });
+  });
 }
 
 function promptInstallStryker() {
@@ -49,30 +50,30 @@ function promptInstallStryker() {
 
 function installStryker() {
   printStrykerASCII();
-  child.execSync('npm i --save-dev stryker stryker-api', {stdio: [0, 1, 2]});
+  child.execSync('npm i --save-dev stryker stryker-api', { stdio: [0, 1, 2] });
   console.log(chalk.green('Stryker installation done.'));
   console.log(`Get started by using ${chalk.blue('`stryker --help`')}.`);
 }
 
 function printStrykerASCII() {
-  const strykerASCII = 
-  '\n' + 
-  chalk.yellow('             |STRYKER|              ') + '\n' + 
-  chalk.yellow('       ~control the mutants~        ') + '\n' + '\n' + 
-  chalk.red('           ..####') + chalk.white('@')  +  chalk.red('####..            ') + '\n' + 
-  chalk.red('        .########') + chalk.white('@') + chalk.red('########.         ') + '\n' + 
-  chalk.red('      .#####################.       ') + '\n' + 
-  chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' + 
-  chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
-  chalk.red('    #########') + chalk.yellow('##') + chalk.red('################     ') + '\n' + 
-  chalk.red('    ') + chalk.white('@@@') + chalk.red('#######') + chalk.yellow('#######') + chalk.red('#######') + chalk.white('@@@') + chalk.red('     ') + '\n' + 
-  chalk.red('    ################') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
-  chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' + 
-  chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' + 
-  chalk.red(`      '######################'      `) + '\n' +  
-  chalk.red(`        '########`) + chalk.white('@') + chalk.red(`#########'        `) + '\n' +  
-  chalk.red(`           ''####`) + chalk.white('@') + chalk.red(`####''            `) + '\n';
+  const strykerASCII =
+    '\n' +
+    chalk.yellow('             |STRYKER|              ') + '\n' +
+    chalk.yellow('       ~control the mutants~        ') + '\n' + '\n' +
+    chalk.red('           ..####') + chalk.white('@') + chalk.red('####..            ') + '\n' +
+    chalk.red('        .########') + chalk.white('@') + chalk.red('########.         ') + '\n' +
+    chalk.red('      .#####################.       ') + '\n' +
+    chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' +
+    chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' +
+    chalk.red('    #########') + chalk.yellow('##') + chalk.red('################     ') + '\n' +
+    chalk.red('    ') + chalk.white('@@@') + chalk.red('#######') + chalk.yellow('#######') + chalk.red('#######') + chalk.white('@@@') + chalk.red('     ') + '\n' +
+    chalk.red('    ################') + chalk.yellow('##') + chalk.red('#########     ') + '\n' +
+    chalk.red('    #########') + chalk.yellow('##') + chalk.red('#####') + chalk.yellow('##') + chalk.red('#########     ') + '\n' +
+    chalk.red('     #########') + chalk.yellow('#######') + chalk.red('#########      ') + '\n' +
+    chalk.red(`      '######################'      `) + '\n' +
+    chalk.red(`        '########`) + chalk.white('@') + chalk.red(`#########'        `) + '\n' +
+    chalk.red(`           ''####`) + chalk.white('@') + chalk.red(`####''            `) + '\n';
   console.log(strykerASCII);
 }
 
-start();
+run();
